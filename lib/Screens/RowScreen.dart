523a5d488ae37a12fce12f3d6a1a5866d1ec5f3e
Werneser <mishaaldeny@gmail.com>
2025-10-04 11:47:21 +0300

    Обновленние Row

--- lib/Screens/RowScreen.dart
+++ lib/Screens/RowScreen.dart
@@ -7,7 +7,7 @@ class RowScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Row Demo'),
+        title: const Text('Row Screen'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
